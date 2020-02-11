82ecc55846ba8bc729a4364fe91bbe58485f2ee4
nikkesan <nikke.san@gmail.com>
2020-02-11 12:05:12 +0000

    add main joy image to landing page component

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,1 +1,14 @@
-// landing page
+import React from "react";
+import { ReactComponent as LogoSvg } from "../../svgs/logo.svg";
+import { ReactComponent as LandingPageSvg } from "../../svgs/landing-page.svg";
+
+const LandingPage = () => {
+  return (
+    <div>
+      {/* <LogoSvg /> */}
+      <LandingPageSvg />
+    </div>
+  );
+};
+
+export default LandingPage;
