19be3164b31ead3d772a511086d733fc02710a19
Maria <m.olanipekun@hotmail.com>
2020-02-11 13:08:10 +0000

    refactor db_build.js file

--- server/model/database/db_build.js
+++ server/model/database/db_build.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
-const db_build = cb => dbConnection.query(sql, cb);
+db_build(sql)
+  .then(console.log)
+  .catch(console.log);
 
 module.exports = db_build;
+
