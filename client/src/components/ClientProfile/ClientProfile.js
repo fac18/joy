347694e47b3e8467f1edb132faa87cb1e1c9a86f
nikkesan <nikke.san@gmail.com>
2020-02-12 14:08:24 +0000

    set up client profile page template

--- client/src/components/ClientProfile/ClientProfile.js
+++ client/src/components/ClientProfile/ClientProfile.js
@@ -3,22 +3,31 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../../theme";
-
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+// import Button from "../Button/Button";
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
+    minWidth: 350,
     maxWidth: 500,
     border: "solid 2px #A0B43B",
-    textAlign: "center"
+    textAlign: "center",
+    margin: "auto",
+    margin: "1rem"
   },
-  title: {
-    fontSize: 14
+  services: {
+    minWidth: 350,
+    maxWidth: 500,
+    border: "solid 2px #E91E63",
+    margin: "1rem"
+  },
+  mainTitle: {
+    paddingTop: "2rem"
   },
   pos: {
     display: "inline-block",
@@ -28,34 +37,96 @@ const useStyles = makeStyles({
     borderRadius: "50",
     padding: "13px 28px",
     borderRadius: "100px"
+  },
+  greenButton: {
+    background: "#A0B43B",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#f06292"
+    },
+    padding: "10px 20px",
+    margin: "0.5rem auto"
+  },
+  pinkButton: {
+    background: "#E71F67",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#f06292"
+    },
+    padding: "10px 20px",
+    margin: "0.5rem auto"
   }
 });
 
 const ClientProfile = () => {
   const classes = useStyles();
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textPrimary" gutterBottom>
-          Initial wellbeing score: 8
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography className={classes.mainTitle} variant="h4" gutterBottom>
+          <AccountCircleIcon fontSize="large" />
+          Jim Brown, 64
         </Typography>
-        <Typography variant="h6" component="h2">
-          Current wellbeing score:
-        </Typography>
-        <Typography className={classes.pos} color="textPrimary">
-          8
-        </Typography>
-        <Typography variant="body2" component="p">
-          Last assessment date: 3 Jan 2020
-        </Typography>
-        <Typography variant="h6" component="h2">
-          Next assessment due: 3 Apr 2020
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Start Wellbeing Assesment</Button>
-      </CardActions>
-    </Card>
+
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography
+              variant="body1"
+              component="p"
+              color="textPrimary"
+              gutterBottom
+            >
+              Initial wellbeing score: <b>8</b>
+            </Typography>
+            <Typography variant="h6" component="h6">
+              <b>Current wellbeing score:</b>
+            </Typography>
+            <Typography className={classes.pos} color="textPrimary">
+              8
+            </Typography>
+            <Typography variant="body1" component="p">
+              Last assessment date: 3 Jan 2020
+            </Typography>
+            <Typography variant="h6" component="h6">
+              <b>Next assessment due: 3 Apr 2020</b>
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              className={classes.greenButton}
+              variant="container"
+              size="medium"
+            >
+              Start Wellbeing Assessment
+            </Button>
+          </CardActions>
+        </Card>
+        <Card className={classes.services}>
+          <CardContent>
+            <Typography variant="h6" component="h2">
+              <b>Current services referred to:</b>
+            </Typography>
+            <Typography color="textPrimary">
+              Learning Spanish
+              <br />
+              Pub Lunch
+              <br />
+              Modern Dance
+              <br />
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              className={classes.pinkButton}
+              variant="container"
+              size="medium"
+            >
+              Start Services Referral
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+    </ThemeProvider>
   );
 };
 
