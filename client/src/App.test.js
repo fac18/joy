4665946e27a7b7ffdc33d4c664beae4cf6d3614b
Roshan Thaabith <roshlarosh@gmail.com>
2020-02-13 15:50:22 +0000

    add some fail tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,12 +2,19 @@
 // import { render } from "@testing-library/react";
 // import App from "./App";
 
-test("Jest is working", () => {
-  expect(true).toBeTruthy();
-});
+// test("Jest is working", () => {
+//   expect(true).toBeTruthy();
+// });
 
 // test("renders learn react link", () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+it("renders without crashing", () => {
+  shallow(<App />);
+});
--- client/src/components/ClientProfile/ClientProfile.test.js
+++ client/src/components/ClientProfile/ClientProfile.test.js
@@ -1,3 +1,29 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import ClientProfile from './ClientProfile';
+// describe('ClientProfile', () => {
+//   it('should render correctly in "debug" mode', () => {
+//     const component = shallow(<ClientProfile debug />);
+
+//     expect(component).toMatchSnapshot();
+//   });
+// });
+
+// import { createRender } from "@material-ui/core/test-utils";
+
+// describe("<ClientProfile />", () => {
+//   let render;
+//   let wrapper;
+
+//   before(() => {
+//     render = createRender();
+//   });
+
+//   it("should work", () => {
+//     wrapper = render(<ClientProfile />);
+//   });
+// });
+
 test("Jest is working", () => {
   expect(true).toBeTruthy();
 });
