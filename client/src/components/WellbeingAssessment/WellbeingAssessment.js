367565800d4ad49e763b1dd2a15807ecdaa5d195
Roshan Thaabith <roshlarosh@gmail.com>
2020-02-17 15:30:54 +0000

    form with radio buttons

--- client/src/components/WellbeingAssessment/WellbeingAssessment.js
+++ client/src/components/WellbeingAssessment/WellbeingAssessment.js
@@ -1,23 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useState, useInput } from "react";
 import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import theme from "../../theme";
 import NavBar from "../NavBar/NavBar";
 import Typography from "@material-ui/core/Typography";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+import "./WellbeingAssessment.css";
 
 const useStyles = makeStyles({
-  //   assessmentCard: {
-  //     backgroundColor: "#EBEDEE",
-  //     width: "600px",
-  //     height: "600px",
-  //     display: "flex",
-  //     borderRadius: "10px",
-  //     display: "flex",
-  //     alignContent: "center"
-  //   },
   mainTitle: {
     fontSize: "35px",
     fontWeight: "bold",
@@ -35,6 +23,9 @@ const useStyles = makeStyles({
 
 const WellbeingAssessment = () => {
   const classes = useStyles();
+  const [state, setState] = useState("");
+  const handleChange = event => setState(event.target.value);
+
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
@@ -49,9 +40,137 @@ const WellbeingAssessment = () => {
         <b>Overall, how often do you:</b> (please select)
       </Typography>
       <br />
-      <Typography className={classes.startQ}>
-        <b>1. Feel lack of companionship?</b>
-      </Typography>
+      <form className="formWellbeing">
+        <fieldset id="question1">
+          <h4>
+            1. Feel lack of companionship?
+            <br />
+            <label>
+              <input
+                type="radio"
+                value="1"
+                id="1"
+                name="question1"
+                className="wellbeingQ"
+                checked={state === "1"}
+                onChange={handleChange}
+              />
+              <span>1</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="2"
+                id="2"
+                name="question1"
+                className="wellbeingQ"
+                checked={state === "2"}
+                onChange={handleChange}
+              />
+              <span>2</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="3"
+                id="3"
+                name="question1"
+                className="wellbeingQ"
+                checked={state === "3"}
+                onChange={handleChange}
+              />
+              <span>3</span>
+            </label>
+          </h4>
+        </fieldset>
+        <fieldset id="question2">
+          <h4>
+            2. Feel left out?
+            <br />
+            <label>
+              <input
+                type="radio"
+                value="1"
+                id="1"
+                name="question2"
+                className="wellbeingQ"
+                checked={state === "1"}
+                onChange={handleChange}
+              />
+              <span>1</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="2"
+                id="2"
+                name="question2"
+                className="wellbeingQ"
+                checked={state === "2"}
+                onChange={handleChange}
+              />
+              <span>2</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="3"
+                id="3"
+                name="question2"
+                className="wellbeingQ"
+                checked={state === "3"}
+                onChange={handleChange}
+              />
+              <span>3</span>
+            </label>
+          </h4>
+        </fieldset>
+        <fieldset id="question3">
+          <h4>
+            3. Feel isolated from others?
+            <br />
+            <label>
+              <input
+                type="radio"
+                value="1"
+                id="1"
+                name="question3"
+                className="wellbeingQ"
+                checked={state === "1"}
+                onChange={handleChange}
+              />
+              <span>1</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="2"
+                id="2"
+                name="question3"
+                className="wellbeingQ"
+                checked={state === "2"}
+                onChange={handleChange}
+              />
+              <span>2</span>
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="3"
+                id="3"
+                name="question3"
+                className="wellbeingQ"
+                checked={state === "3"}
+                onChange={handleChange}
+              />
+              <span>3</span>
+            </label>
+          </h4>
+        </fieldset>
+        <div>
+          <button type="submit">NEXT</button>
+        </div>
+      </form>
     </ThemeProvider>
   );
 };
