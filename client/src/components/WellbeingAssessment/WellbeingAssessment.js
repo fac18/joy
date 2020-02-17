4b63147d40397bfcc867115e986f8adc450888fd
Roshan Thaabith <roshlarosh@gmail.com>
2020-02-17 20:56:12 +0000

    add style to wellbeing assessment page

--- client/src/components/WellbeingAssessment/WellbeingAssessment.js
+++ client/src/components/WellbeingAssessment/WellbeingAssessment.js
@@ -1,11 +1,12 @@
 // initial assessment
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
 import theme from "../../theme";
 import NavBar from "../NavBar/NavBar";
 import Typography from "@material-ui/core/Typography";
 import "./WellbeingAssessment.css";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
   mainTitle: {
@@ -20,12 +21,24 @@ const useStyles = makeStyles({
   startQ: {
     fontSize: "20px",
     textAlign: "center"
+  },
+  pinkButton: {
+    background: "#E71F67",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#80902F"
+    },
+    padding: "10px 30px",
+    marginLeft: "11rem",
+    marginTop: "2rem",
+    marginBottom: "2rem"
   }
 });
 
 const ClientAssessment = () => {
   const classes = useStyles();
   const { register, handleSubmit, errors } = useForm();
+
   const onSubmit = data => {
     console.log(data);
   };
@@ -58,6 +71,7 @@ const ClientAssessment = () => {
               required
               value="1"
               ref={register}
+              className="radio-custom"
             />
           </label>
           <label>
@@ -81,6 +95,11 @@ const ClientAssessment = () => {
             />
           </label>
         </fieldset>
+        <aside className="ratingQ">
+          <span className="one">Hardly ever </span>
+          <span className="two">Sometimes </span>
+          <span className="three">Often</span>
+        </aside>
         {/* { errors.companionship && <p>Required field!</p> } */}
         <h4> 2. Feel left out?</h4>
         <fieldset className="groupQuestion">
@@ -115,6 +134,11 @@ const ClientAssessment = () => {
             />
           </label>
         </fieldset>
+        <aside className="ratingQ">
+          <span className="one">Hardly ever </span>
+          <span className="two">Sometimes </span>
+          <span className="three">Often</span>
+        </aside>
         <h4> 3. Feel isolated from others?</h4>
         <fieldset className="groupQuestion">
           <label>
@@ -148,24 +172,81 @@ const ClientAssessment = () => {
             />
           </label>
         </fieldset>
+        <aside className="ratingQ">
+          <span className="one">Hardly ever </span>
+          <span className="two">Sometimes </span>
+          <span className="three">Often</span>
+        </aside>
         <br />
-        <label>
-          Additional Notes:
-          <textarea name="additionalNotes" ref={register} />
-        </label>
         <br />
-        <label>
-          Next Appointment:
-          <input
-            type="datetime-local"
-            placeholder="nextAppointment"
-            name="nextAppointment"
+        <h4>Additional Notes:</h4>
+        <label className="additionalNotes">
+          <br />
+          <br />
+          <textarea
+            placeholder="The client’s current thoughts and feelings and Anything else to be aware of?"
+            name="additionalNotes"
             ref={register}
           />
         </label>
         <br />
+        <h4>Schedule next Appointment:</h4>
+        <label>
+          1 month
+          <input
+            name="schedule"
+            type="radio"
+            required
+            value="1 month"
+            ref={register}
+          />
+        </label>
+        <label>
+          3 month
+          <input
+            name="schedule"
+            type="radio"
+            required
+            value="3 month"
+            ref={register}
+          />
+        </label>
+        <label>
+          6 month
+          <input
+            name="schedule"
+            type="radio"
+            required
+            value="6 month"
+            ref={register}
+          />
+        </label>
+        <label>
+          Other
+          <input
+            name="schedule"
+            type="radio"
+            required
+            value="other"
+            ref={register}
+          />
+        </label>
+        {/* <input
+          type="datetime-local"
+          placeholder="nextAppointment"
+          name="nextAppointment"
+          ref={register}
+        /> */}
+        <br />
 
-        <input type="submit" />
+        <Button
+          type="submit"
+          className={classes.pinkButton}
+          variant="container"
+          size="medium"
+        >
+          NEXT
+        </Button>
       </form>
     </ThemeProvider>
   );
