6f070b3e5c1a068f609cbffb1aae975a374f4aef
Maria <m.olanipekun@hotmail.com>
2020-02-19 11:23:51 +0000

    add input fields for register client form

--- client/src/components/RegisterClient/RegisterClient.js
+++ client/src/components/RegisterClient/RegisterClient.js
@@ -38,5 +38,78 @@ const RegisterClient = () => {
     nhsNumber,
   } = state;
 
-  
+  return (
+    <form>
+
+      <label for="firstName">First Name</label>
+      <input
+        type="text"
+        id="firstName"
+        name="firstName"
+        defaultValue={firstName}
+        required
+        onChange={onChange}
+      />
+
+      <label for="lastName">Last Name</label>
+      <input
+        type="text"
+        name="lastName"
+        id="lastName"
+        defaultValue={lastName}
+        required
+        onChange={onChange}
+      />
+
+      <label for="knownAs">Known As</label>
+      <input
+        type="text"
+        id="knowsAs"
+        name="knownAs"
+        defaultValue={knownAs}
+        required
+        onChange={onChange}
+      />
+
+      <label for="dateOfBirth">Date Of Birth</label>
+      <input
+        type="date"
+        name="dateOfBirth"
+        id="dateOfBirth"
+        defaultValue={dateOfBirth}
+        required
+        onChange={onChange}
+      />
+
+      <label for="phoneNumber">Phone Number</label>
+      <input
+        type="number"
+        name="phoneNumber"
+        id="phoneNumber"
+        defaultValue={phoneNumber}
+        onChange={onChange}
+      />
+
+      <label for="address">Address</label>
+      <input
+        type="text"
+        id="address"
+        name="address"
+        defaultValue={address}
+        onChange={onChange}
+      />
+
+      <label for="nhsNumber">NHS Number</label>
+      <input
+        type="text"
+        id="nhsNumber"
+        name="nhsNumber"
+        defaultValue={nhsNumber}
+        onChange={onChange}
+      />
+
+      <button type="submit">Register Client</button>
+    </form>
+  );
+};
 export default RegisterClient;
