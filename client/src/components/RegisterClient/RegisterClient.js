21b93fd19b44d6d09e7cb22662d6b679beee6f99
Maria <m.olanipekun@hotmail.com>
2020-02-19 11:13:50 +0000

    add register client state

--- client/src/components/RegisterClient/RegisterClient.js
+++ client/src/components/RegisterClient/RegisterClient.js
@@ -1,1 +1,42 @@
-// register client
+import React, { useReducer } from "react";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  knownAs: "",
+  email: "",
+  password1: "",
+  password2: "",
+  location: ""
+};
+
+function reducer(state, { field, value }) {
+  return {
+    ...state,
+    [field]: value
+  };
+}
+
+const RegisterClient = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // if(state) {
+  //     console.log(state);
+  // }
+
+  const onChange = e => {
+    dispatch({ field: e.target.name, value: e.target.value });
+  };
+
+  const {
+      firstName,
+      lastName,
+      knownAs,
+    dateOfBirth,
+    phoneNumber,
+    address,
+    nhsNumber,
+  } = state;
+
+  
+export default RegisterClient;
