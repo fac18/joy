323bb589a316925c2e92ed4af5fef9814d75c4f5
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-02-20 22:19:34 +0000

    Amend ucla3 calculation

--- server/model/database/db_build.js
+++ server/model/database/db_build.js
@@ -1,15 +1,15 @@
-const dbConnection = require('./db_connection')
-const fs = require('fs')
+const dbConnection = require('./db_connection');
+const fs = require('fs');
 
-const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString()
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 // db_build(sql)
 //   .then(console.log)
 //   .catch(console.log);
 
 dbConnection.query(sql, (err, res) => {
-  if (err) throw err
-  console.log('Tables created with results: ', res)
-})
+  if (err) throw err;
+  console.log('Tables created with results: ', res);
+});
 
 // module.exports = db_build;
