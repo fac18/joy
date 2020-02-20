bdc4e9928dd5809f82b9c2cd46d2c70c203db639
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-02-20 14:31:36 +0000

    Fix merge conflict

--- server/model/database/db_connection.js
+++ server/model/database/db_connection.js
@@ -1,27 +1,27 @@
-const { Pool } = require("pg");
-const url = require("url");
-const dotenv = require("dotenv").config();
+const { Pool } = require('pg');
+const url = require('url');
+const dotenv = require('dotenv').config();
 
-let DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 console.log(DATABASE_URL);
 
-if (!DATABASE_URL) throw new Error("Environment variable not available");
+if (!DATABASE_URL) throw new Error('Environment variable not available');
 
 const params = url.parse(DATABASE_URL);
 
-const [username, password] = params.auth.split(":");
+const [username, password] = params.auth.split(':');
 
 // The below commented out code and params split do the same thing
-console.log("This is the param path", params.pathname.split("/")[1]);
+console.log('This is the param path', params.pathname.split('/')[1]);
 
 const options = {
   host: params.hostname,
   port: params.port,
-  database: params.pathname.split("/")[1],
+  database: params.pathname.split('/')[1],
   max: process.env.DB_MAX_CONNECTIONS || 2,
   user: username,
   password,
-  ssl: params.hostname !== "localhost"
+  ssl: params.hostname !== 'localhost'
 };
 
 module.exports = new Pool(options);
