1593b8fd8e92e1b45b7b6b0b5988920f7433c4c0
nikkesan <nikke.san@gmail.com>
2020-02-20 20:30:45 +0000

    clean up dashboard files

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,7 +1,5 @@
 import React from "react";
-import ServicesGraph from "./ServicesGraph";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
 import NavBar from "../NavBar/NavBar";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "../../theme";
@@ -16,21 +14,10 @@ const useStyles = makeStyles({
     textAlign: "center",
     fontFamily: "Source Sans Pro"
   },
-  card: {
-    minWidth: 275,
-    maxWidth: 500,
-    backgroundColor: "#EBEDEE",
-    margin: "auto",
-    marginTop: 25,
-    padding: 20,
-    color: "#676767",
-    display: "flex",
-    flexDirection: "row",
-    fontSize: 20
-  },
   emphasis: {
     fontSize: 40,
-    color: "#E71F67"
+    color: "#E71F67",
+    margin: "1rem"
   }
 });
 
@@ -69,9 +56,6 @@ const Dashboard = ({
           servicesPopularity={servicesPopularity}
           setServicesPopularity={setServicesPopularity}
         />
-        <Card className={classes.card}>
-          <ServicesGraph />
-        </Card>
       </div>
     </ThemeProvider>
   );
