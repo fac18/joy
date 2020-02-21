a32c49daace2387ec60945d7418b0ff15281ac7e
nikkesan <nikke.san@gmail.com>
2020-02-21 01:25:36 +0000

    add styling to register client

--- client/src/components/RegisterClient/RegisterClient.js
+++ client/src/components/RegisterClient/RegisterClient.js
@@ -7,21 +7,46 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import NavBar from "../NavBar/NavBar";
+import Input from "@material-ui/core/Input";
+import Checkbox from "@material-ui/core/Checkbox";
+import Chip from "@material-ui/core/Chip";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const useStyles = makeStyles({
   root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: 200
-    },
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     minWidth: 350,
     maxWidth: 500,
     border: "solid 2px #E91E63",
+    borderRadius: "10px",
     textAlign: "center",
-    margin: "1rem"
+    margin: "1rem",
+    padding: "2rem"
+  },
+  selectBox: {
+    width: 500,
+    "& > * + *": {
+      marginTop: theme.spacing(3)
+    }
   },
   mainTitle: {
     paddingTop: "2rem"
+  },
+  textField: {
+    width: 300,
+    marginBottom: "1rem"
+  },
+  pinkButton: {
+    background: "#E71F67",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#a11548"
+    },
+    padding: "10px 20px",
+    margin: "0.5rem auto"
   }
 });
 
@@ -45,6 +70,11 @@ function reducer(state, { field, value }) {
 const RegisterClient = () => {
   const classes = useStyles();
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const [checked, setChecked] = React.useState(true);
+  const handleCheckBoxChange = event => {
+    setChecked(event.target.checked);
+  };
 
   // if(state) {
   //     console.log(state);
@@ -92,6 +122,17 @@ const RegisterClient = () => {
     areasOfSupport
   } = state;
 
+  const filterOptions = [
+    { option: "Feeling Lonely/Isolated" },
+    { option: "Managing a health condition" },
+    { option: "Help with Money" },
+    { option: "Getting outdoors" },
+    { option: "Improving Fitness" },
+    { option: "Information and advice" },
+    { option: "Managing a health condition" },
+    { option: "Assistance with day-to-day tasks" }
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <NavBar />
@@ -99,8 +140,130 @@ const RegisterClient = () => {
         <Typography className={classes.mainTitle} variant="h4" gutterBottom>
           Register new client:
         </Typography>
-        <form onSubmit={handleSubmit}>
-          <label for="firstName">First Name</label>
+
+        <form
+          className={classes.root}
+          onSubmit={handleSubmit}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            className={classes.textField}
+            label="First name"
+            type="text"
+            id="firstName"
+            name="firstName"
+            defaultValue={firstName}
+            required
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.textField}
+            label="Surname"
+            type="text"
+            name="lastName"
+            id="lastName"
+            defaultValue={lastName}
+            required
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.textField}
+            label="Known as"
+            type="text"
+            id="knowsAs"
+            name="knownAs"
+            defaultValue={knownAs}
+            required
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.textField}
+            label="Date of Birth"
+            type="date"
+            name="dateOfBirth"
+            id="dateOfBirth"
+            defaultValue={dateOfBirth}
+            required
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.textField}
+            label="Phone number"
+            type="text"
+            name="phoneNumber"
+            id="phoneNumber"
+            defaultValue={phoneNumber}
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.textField}
+            label="Address"
+            type="textarea"
+            id="address"
+            name="address"
+            defaultValue={address}
+            onChange={onChange}
+          />
+          <TextField
+            className={classes.textField}
+            label="NHS number"
+            type="text"
+            id="nhsNumber"
+            name="nhsNumber"
+            defaultValue={nhsNumber}
+            onChange={onChange}
+          />
+          <label>
+            I consent to Joy storing and processing my personal data
+          </label>
+          <Checkbox
+            className={classes.textField}
+            checked={checked}
+            onChange={handleCheckBoxChange}
+            value="primary"
+            inputProps={{ "aria-label": "primary checkbox" }}
+            id="consent"
+            name="consent"
+            defaultValue={true}
+            // onChange={onChange}
+          />
+          <label for="areasOfSupport">
+            What areas does the client need support with?
+          </label>
+          <div className={classes.selectBox}>
+            <Autocomplete
+              className={classes.textField}
+              multiple
+              id="tags-outlined"
+              options={filterOptions}
+              getOptionLabel={option => option.option}
+              // defaultValue={[filterOptions[0]]}
+              filterSelectedOptions
+              renderInput={params => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  label="Please select"
+                  placeholder="Support areas"
+                  fullWidth
+                />
+              )}
+            />
+          </div>
+          <Button
+            className={classes.pinkButton}
+            variant="container"
+            size="medium"
+            type="submit"
+          >
+            Register Client
+          </Button>
+        </form>
+
+        {/* <form onSubmit={handleSubmit}> */}
+        {/* <label for="firstName">First Name</label>
+
           <input
             type="text"
             id="firstName"
@@ -109,9 +272,8 @@ const RegisterClient = () => {
             required
             onChange={onChange}
           />
-          <br />
-
-          <label for="lastName">Last Name</label>
+          <br /> */}
+        {/* <label for="lastName">Last Name</label>
           <input
             type="text"
             name="lastName"
@@ -120,9 +282,8 @@ const RegisterClient = () => {
             required
             onChange={onChange}
           />
-          <br />
-
-          <label for="knownAs">Known As</label>
+          <br /> */}
+        {/* <label for="knownAs">Known As</label>
           <input
             type="text"
             id="knowsAs"
@@ -131,9 +292,8 @@ const RegisterClient = () => {
             required
             onChange={onChange}
           />
-          <br />
-
-          <label for="dateOfBirth">Date Of Birth</label>
+          <br /> */}
+        {/* <label for="dateOfBirth">Date Of Birth</label>
           <input
             type="date"
             name="dateOfBirth"
@@ -142,9 +302,8 @@ const RegisterClient = () => {
             required
             onChange={onChange}
           />
-          <br />
-
-          <label for="phoneNumber">Phone Number</label>
+          <br /> */}
+        {/* <label for="phoneNumber">Phone Number</label>
           <input
             type="text"
             name="phoneNumber"
@@ -152,18 +311,16 @@ const RegisterClient = () => {
             defaultValue={phoneNumber}
             onChange={onChange}
           />
-          <br />
-
-          <label for="address">Address</label>
+          <br /> */}
+        {/* <label for="address">Address</label>
           <input
             type="textarea"
             id="address"
             name="address"
             defaultValue={address}
             onChange={onChange}
-          />
-
-          <label for="nhsNumber">NHS Number</label>
+          /> */}
+        {/* <label for="nhsNumber">NHS Number</label>
           <input
             type="text"
             id="nhsNumber"
@@ -171,9 +328,8 @@ const RegisterClient = () => {
             defaultValue={nhsNumber}
             onChange={onChange}
           />
-          <br />
-
-          <label for="nhsNumber">
+          <br /> */}
+        {/* <label for="nhsNumber">
             I consent to Joy storing and processing my personal data
           </label>
           <input
@@ -182,38 +338,34 @@ const RegisterClient = () => {
             name="consent"
             defaultValue={true}
             onChange={onChange}
-          />
-          <br />
-
-          <label for="areasOfSupport">
+          /> */}
+        {/* <br /> */}
+        {/* <label for="areasOfSupport">
             What areas does the client need support with?
-          </label>
-          <select
-            name="areasOfSupport"
-            multiple
-            id="areasOfSupport"
-            defaultValue={areasOfSupport}
-            onChange={onChange}
-          >
-            <option value="Feeling Lonely/Isolated">
-              Feeling Lonely/Isolated
-            </option>
-            <option value="Managing a health condition">
-              Managing a health condition
-            </option>
-            <option value="Help with Money">Help with Money</option>
-            <option value="Getting outdoors">Getting outdoors</option>
-            <option value="Improving Fitness">Improving Fitness</option>
-            <option value="Information and advice">
-              Information and advice
-            </option>
-            <option value="Assistance with day-to-day tasks">
-              Assistance with day-to-day tasks
-            </option>
-          </select>
-
-          <button type="submit">Register Client</button>
-        </form>
+          </label> */}
+        {/* <select
+          name="areasOfSupport"
+          multiple
+          id="areasOfSupport"
+          defaultValue={areasOfSupport}
+          onChange={onChange}
+        >
+          <option value="Feeling Lonely/Isolated">
+            Feeling Lonely/Isolated
+          </option>
+          <option value="Managing a health condition">
+            Managing a health condition
+          </option>
+          <option value="Help with Money">Help with Money</option>
+          <option value="Getting outdoors">Getting outdoors</option>
+          <option value="Improving Fitness">Improving Fitness</option>
+          <option value="Information and advice">Information and advice</option>
+          <option value="Assistance with day-to-day tasks">
+            Assistance with day-to-day tasks
+          </option>
+        </select>
+        {/* <button type="submit">Register Client</button> */}
+        {/* </form> */}
       </div>
     </ThemeProvider>
   );
