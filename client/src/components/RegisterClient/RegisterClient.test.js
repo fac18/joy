cb96f96528c5382e9c73bcd784b392e60858c172
Rosalie-Baxter <rosalie.baxter@outlook.com>
2020-02-23 15:25:57 +0000

    Remove register client test

--- client/src/components/RegisterClient/RegisterClient.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import RegisterClient from './RegisterClient';
-
-// test("renders the home register client component", () => {
-//   render(<RegisterClient />);
-// });
