05e6eaa1dbe423fef295981a524e117ca891bcf0
Roshan Thaabith <roshlarosh@gmail.com>
2020-02-24 17:39:48 +0000

    add codecov yml file

--- client/src/components/RegisterClient/RegisterClient.test.js
+++ client/src/components/RegisterClient/RegisterClient.test.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import { render } from "@testing-library/react";
 import RegisterClient from "./RegisterClient";
-
-test("renders the home register client component", () => {
-  render(<RegisterClient />);
+test("register client button is on the page", () => {
+  const history = createMemoryHistory();
+  const { debug, getByText } = render(
+    <Router history={history}>
+      <RegisterClient />
+    </Router>
+  );
 });
