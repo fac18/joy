40ca8f18f2915541321f1c85ad1ba45591ba30ff
Maria <m.olanipekun@hotmail.com>
2020-02-25 17:43:28 +0000

    update search client test

--- client/src/components/SearchClient/SearchClient.test.js
+++ client/src/components/SearchClient/SearchClient.test.js
@@ -1,11 +1,7 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import SearchClient from "./SearchClient";
+import React from "react";
+import { render } from "@testing-library/react";
+import SearchClient from "./SearchClient";
 
-// test("renders the home search client component", () => {
-//   render(<SearchClient />);
-// });
-
-test("Jest is working", () => {
-  expect(true).toBeTruthy();
+test("renders the client search page component", () => {
+  render(<SearchClient />);
 });
