40fc4f2e542d36e509916c372c58bdcc8365f849
Maria <m.olanipekun@hotmail.com>
2020-02-25 17:29:28 +0000

    add loading page test file

---
+++ client/src/components/LoadingPage/LoadingPage.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import LoadingPage from "./LoadingPage";
+
+test("renders the home wellbeing assessment component", () => {
+  render(<LoadingPage />);
+});
