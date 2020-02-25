f802272cd421dcd7b14a048f120c723175385c4f
Maria <m.olanipekun@hotmail.com>
2020-02-25 17:27:44 +0000

    add totals chart test file for the dashboard

---
+++ client/src/components/Dashboard/TotalsChart.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import TotalsChart from "./TotalsChart";
+
+test("renders the home wellbeing assessment component", () => {
+  render(<TotalsChart />);
+});
