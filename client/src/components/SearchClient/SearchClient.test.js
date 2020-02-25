36fc358250c86b354a4368610fc83067929d4a9e
Maria <m.olanipekun@hotmail.com>
2020-02-25 18:38:28 +0000

    add imports for test files

--- client/src/components/SearchClient/SearchClient.test.js
+++ client/src/components/SearchClient/SearchClient.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import SearchClient from "./SearchClient";
+import { filterClients } from '../../utils/filterClients';
 
 test("renders the client search page component", () => {
   render(<SearchClient />);
--- client/src/components/WellbeingAssessment/WellbeingAssessment.test.js
+++ client/src/components/WellbeingAssessment/WellbeingAssessment.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import { render } from "@testing-library/react";
 import WellbeingAssessmentModal from "./WellbeingAssessmentModal";
 import WellbeingAssessment from "./WellbeingAssessment";
+import { useParams, useHistory } from 'react-router-dom';
+
+let { id } = useParams();
+const history = useHistory();
 
 test("renders the wellbeing assessment page component", () => {
   render(<WellbeingAssessment />);
