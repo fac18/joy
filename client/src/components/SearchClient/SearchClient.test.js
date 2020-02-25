0997123205ab25e771b520d6013b7f97d00a0a96
Maria <m.olanipekun@hotmail.com>
2020-02-25 19:08:58 +0000

    fix tests/travis

--- client/src/components/SearchClient/SearchClient.test.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import SearchClient from "./SearchClient";
-// import { filterClients } from '../../utils/filterClients';
-
-// test("renders the client search page component", () => {
-//   render(<SearchClient />);
-// });
--- client/src/components/WellbeingAssessment/WellbeingAssessment.test.js
+++ client/src/components/WellbeingAssessment/WellbeingAssessment.test.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import WellbeingAssessmentModal from "./WellbeingAssessmentModal";
-// import WellbeingAssessment from "./WellbeingAssessment";
-
-// let { id } = useParams();
-// const history = useHistory();
-
-// test("renders the wellbeing assessment page component", () => {
-//   render(<WellbeingAssessment />);
-// });
 
 test("renders the home wellbeing assessment modal", () => {
   render(<WellbeingAssessmentModal />);
