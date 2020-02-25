a14d771b30fc745e41b13db29675192c303fd72d
Maria <m.olanipekun@hotmail.com>
2020-02-25 17:25:02 +0000

    add button test file, update client profile test

--- client/src/components/Button/Button.test.js
+++ client/src/components/Button/Button.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import ClientProfile from "./ClientProfile";
+import Button from "./Button";
 
 test("renders the home wellbeing assessment component", () => {
-  render(<ClientProfile />);
+  render(<Button />);
 });
