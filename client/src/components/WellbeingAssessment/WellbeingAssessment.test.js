8398f019c337f7c5ed9ee5445235c2cc09dd2398
Maria <m.olanipekun@hotmail.com>
2020-02-25 17:46:08 +0000

    update wellbeing assessment test

--- client/src/components/WellbeingAssessment/WellbeingAssessment.test.js
+++ client/src/components/WellbeingAssessment/WellbeingAssessment.test.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import WellbeingAssessmentModal from "./WellbeingAssessmentModal";
+import WellbeingAssessment from "./WellbeingAssessment";
 
-test("renders the home wellbeing assessment component", () => {
+test("renders the wellbeing assessment page component", () => {
+  render(<WellbeingAssessment />);
+});
+
+test("renders the home wellbeing assessment modal", () => {
   render(<WellbeingAssessmentModal />);
 });
 
-// test('Jest is working', () => {
-//   expect(true).toBeTruthy();
-// });
